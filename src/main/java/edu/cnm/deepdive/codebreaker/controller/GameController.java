4756f22e09e9c8e2f4e50a65d10f53cdd0af8411
oliver <ogarrott@cnm.edu>
2024-07-01 14:39:15 -0600

    Game can be started.

--- src/main/java/edu/cnm/deepdive/codebreaker/controller/GameController.java
+++ src/main/java/edu/cnm/deepdive/codebreaker/controller/GameController.java
@@ -1,10 +1,13 @@
 package edu.cnm.deepdive.codebreaker.controller;
 
 import edu.cnm.deepdive.codebreaker.model.entity.Game;
+import edu.cnm.deepdive.codebreaker.service.AbstractGameService;
 import edu.cnm.deepdive.codebreaker.service.AbstractUserService;
 import edu.cnm.deepdive.codebreaker.service.UserService;
+import jakarta.validation.Valid;
 import java.util.UUID;
 import org.springframework.http.MediaType;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,26 +17,28 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/games")
+@Validated
 public class GameController {
 
+  private final AbstractGameService gameService;
   private final AbstractUserService userService;
 
   // TODO: 7/1/24 Declare and initialize in the constructor any dependencies.
 
-  public GameController(AbstractUserService userService) {
+  public GameController(AbstractGameService gameService, AbstractUserService userService) {
+    this.gameService = gameService;
     this.userService = userService;
   }
 
   @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-  public Game post(@RequestBody Game game) {
-    // TODO: 7/1/24 Invoke svc method to start game, and return Game instance returned by the service.
-    throw new UnsupportedOperationException();
+  public Game post(@RequestBody @Valid Game game) {
+    // TODO: 7/1/24 Return response with status 201 and location header.
+    return gameService.startGame(game, userService.getCurrentUser());
   }
 
-  @GetMapping(path = "/{key:}") // TODO: 7/1/24 Add regex for key.
+  @GetMapping(path = "/{key}") // TODO: 7/1/24 Add regex for key.
   public Game get(@PathVariable UUID key) {
-    // TODO: 7/1/24 Invoke service method to retrieve specified game instance.
-    throw new UnsupportedOperationException();
+   return gameService.getGame(key, userService.getCurrentUser());
 
   }
 
